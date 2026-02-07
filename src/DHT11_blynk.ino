38c54e91b782ca5f189086a4b538787066edd07b
N.Mahitesh Kumar Reddy <mahiteshkumarreddy03@gmail.com>
2026-02-07 22:06:58 +0530

    feat: Add DHT11 sensor readings sent to Blynk app
    Verified temperature and humidity from DHT11 (pin D3) displayed on Blynk virtual pins V5 & V6.

---
+++ src/DHT11_blynk.ino
@@ -0,0 +1,36 @@
+#define BLYNK_TEMPLATE_ID "TMPL3BNNzz-XS"
+#define BLYNK_TEMPLATE_NAME "Smart Agriculture IoT System "
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <DHT.h>
+
+char auth[] = "hule0hh4YRMqvurv9cKC5zNmKS9PKdi2";
+
+char ssid[] = "VITC-EVENT";
+char pass[] = "Eve@07&08#$";
+
+// DHT11 sensor
+#define DHTPIN D3
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop() {
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
+
+  if (!isnan(temp) && !isnan(hum)) {
+    Blynk.virtualWrite(V5, temp);  
+    Blynk.virtualWrite(V6, hum);   
+  }
+
+  Blynk.run();  
+  delay(2000);
+}
+
